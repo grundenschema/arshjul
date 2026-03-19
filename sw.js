78fa5fec81256be8c9fc3392d2df36ccc997ddb1
grundenschema <grundenschema@gmail.com>
2026-03-19 15:37:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME='grunden-arshjul-v2';
+const APP_SHELL=['/arshjul/','/arshjul/index.html','/arshjul/manifest.webmanifest','/arshjul/icons/icon-192.png','/arshjul/icons/icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):Promise.resolve()))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(req,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match('/arshjul/index.html')))
+  );
+});
